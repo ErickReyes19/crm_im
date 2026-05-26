faf09ab72ce4e8db41c72f096a06a403d49f4e85
ereyes0212 <erick.reyes@tiempo.hn>
2026-05-26 17:31:49 -0600

    cambios

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -16,11 +16,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <SidebarProvider>
       <AppSidebar />
       <SidebarTrigger />
-<<<<<<< HEAD
-      <main className="w-full">
-=======
-      <main className="min-w-0 w-full px-4 md:px-8 overflow-x-hidden">
->>>>>>> ebad26c6d0dab76c906ee4420f1a900b6f4aee37
+      <main className="w-full p-2">
 
         {children}
       </main>
